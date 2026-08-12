27e92456efe47ba7c65b97c308e89242ba57a7dd
Flutter-Code-Craft <muneefullah24mdswe363@gmail.com>
2026-08-11 20:48:14 -0700

    add changes

--- lib/favourite_app.dart
+++ lib/favourite_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:multi_provider/provider_favourite_app.dart';
 import 'package:provider/provider.dart';
+
+import 'package:multi_provider/provider_favourite_app.dart';
 
 class FavouiteApp extends StatefulWidget {
   const FavouiteApp({super.key});
@@ -12,37 +13,60 @@ class FavouiteApp extends StatefulWidget {
 class _FavouiteAppState extends State<FavouiteApp> {
   @override
   Widget build(BuildContext context) {
-
-    final favoriteProvider = Provider.of<FavoriteApp>(context);
-
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-          child: Text("Favourite App"),
+        title: const Text(
+          "Favourite App",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        centerTitle: true,
       ),
+
       body: Column(
         children: [
           Expanded(
             child: ListView.builder(
               itemCount: 100,
               itemBuilder: (context, index) {
-                return ListTile(
-                  onTap: () {
-                    if (favoriteProvider.isFavorite(index)) {
-                      favoriteProvider.removeItem(index);
-                    } else {
-                      favoriteProvider.addItem(index);
-                    }
+                return Consumer<FavoriteApp>(
+                  builder: (context, value, child) {
+                    final isFavorite = value.isFavorite(index);
+
+                    return ListTile(
+                      onTap: () {
+                        if (isFavorite) {
+                          value.removeItem(index);
+                        } else {
+                          value.addItem(index);
+                        }
+                      },
+
+                      leading: CircleAvatar(
+
+                        child: Text(
+                          "$index",
+                        ),
+                      ),
+
+                      title: Text(
+                        "APP $index",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+
+                      trailing: Icon(
+                        isFavorite
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                        color: isFavorite
+                            ? Colors.red
+                            : Colors.grey,
+                      ),
+                    );
                   },
-                  title: Text(
-                    "APP   $index",
-                  ),
-                  trailing: Icon(
-                    favoriteProvider.isFavorite(index)
-                        ? Icons.favorite
-                        : Icons.favorite_border,
-                  ),
                 );
               },
             ),
